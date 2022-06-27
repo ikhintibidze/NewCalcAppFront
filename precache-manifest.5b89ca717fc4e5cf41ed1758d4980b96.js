4afdd8daccac4cbd61739e4c6a60123a5725bf7d
ikakhi <ikakhi@gmail.com>
2022-06-27 23:05:22 +0400

    first commit

---
+++ precache-manifest.5b89ca717fc4e5cf41ed1758d4980b96.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43326eb64ca9ea378203b6a2658ec7e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95c9f7ad66b4e0ee2d94",
+    "url": "/static/css/2.b5ff827b.chunk.css"
+  },
+  {
+    "revision": "8a836f6097755835061d",
+    "url": "/static/css/main.509036e5.chunk.css"
+  },
+  {
+    "revision": "95c9f7ad66b4e0ee2d94",
+    "url": "/static/js/2.52ee170b.chunk.js"
+  },
+  {
+    "revision": "33f401a21d3a104294cedb4a3a01f624",
+    "url": "/static/js/2.52ee170b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a836f6097755835061d",
+    "url": "/static/js/main.b3488753.chunk.js"
+  },
+  {
+    "revision": "405a0d9530fd470eb51a",
+    "url": "/static/js/runtime-main.e562068f.js"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "/static/media/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "/static/media/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "/static/media/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "/static/media/primeicons.e01fd413.eot"
+  }
+]);
